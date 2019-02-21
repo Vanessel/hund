b4092f693d51e47c7f09fc6fc5487f5c003f8591
Vanessa Pertsch <pertsch@kontrollfeld.de>
2019-02-21 10:38:43 +0100

    s oder nicht s

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+  return  gulp.src('./sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+        .pipe(gulp.dest('./css'));
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('./sass/**/*.scss', gulp.series('styles'));
 });
